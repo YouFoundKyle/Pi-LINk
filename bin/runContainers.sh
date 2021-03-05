5f6115b25a0659075622128171b2e1d84497c2ff
Kyle Camacho <15270949+YouFoundKyle@users.noreply.github.com>
2021-03-05 18:34:59 -0500

    Update Utility Scripts (#16)

--- bin/runContainers.sh
+++ bin/runContainers.sh
@@ -1,4 +1,4 @@
 for subdir in `ls /etc/pilink/services`;
 do
-    (sudo -u ha docker-compose -f ./$subdir/docker-compose.yml up --remove-orphans -d);
+    (sudo -u ha docker-compose -f /etc/pilink/services/$subdir/docker-compose.yml up --remove-orphans -d);
 done
