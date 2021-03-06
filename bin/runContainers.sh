1eeca55ef26c8cd88c7d9ce91311c36f47e27a97
Danny <dmayott@gmail.com>
2021-03-06 00:31:33 +0000

    docker fixes

--- bin/runContainers.sh
+++ bin/runContainers.sh
@@ -1,4 +1,4 @@
 for subdir in `ls /etc/pilink/services`;
 do
-    (sudo -u ha docker-compose -f /etc/pilink/services/$subdir/docker-compose.yml up --remove-orphans -d);
+    (sudo docker-compose -f /etc/pilink/services/$subdir/docker-compose.yml up --remove-orphans -d);
 done
