7045bd121553d03d7833fc81fc26cf2b4a53c9bc
Danny <dmayott@gmail.com>
2021-03-06 00:57:53 +0000

    fun fun fun

--- bin/runContainers.sh
+++ bin/runContainers.sh
@@ -1,4 +1,4 @@
 for subdir in `ls /etc/pilink/services`;
 do
-    (sudo docker-compose -f /etc/pilink/services/$subdir/docker-compose.yml up --remove-orphans -d);
+    (docker-compose -f /etc/pilink/services/$subdir/docker-compose.yml up --remove-orphans -d);
 done
