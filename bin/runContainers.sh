ecdcb1f3ce70cd343085d134d4152c71abc95c4f
Kyle <15270949+YouFoundKyle@users.noreply.github.com>
2021-03-05 19:47:15 -0500

    Fix HA User

--- bin/runContainers.sh
+++ bin/runContainers.sh
@@ -1,4 +1,4 @@
 for subdir in `ls /etc/pilink/services`;
 do
-    (sudo -u ha docker-compose -f /etc/pilink/services/$subdir/docker-compose.yml up --remove-orphans -d);
+    (docker-compose -f /etc/pilink/services/$subdir/docker-compose.yml up --remove-orphans -d);
 done
