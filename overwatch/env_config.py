389d1b9a2e35d8c46ea31dc8ece6ce63bd3c3236
Kyle <15270949+YouFoundKyle@users.noreply.github.com>
2021-03-25 13:07:45 -0400

    add end file

---
+++ overwatch/env_config.py
@@ -0,0 +1,6 @@
+PILINK_PATH='/etc/pilink'
+OVERWATCH_FOLDER='/overwatch/'
+SERVICE_PATH= PILINK_PATH + OVERWATCH_FOLDER
+LEASES_PATH= '/var/lib/dhcp/dhcpd.leases'
+NEW_LEASES_FILE='new_leases.pkl'
+OLD_LEASES_FILE='old_leases.pk1'
