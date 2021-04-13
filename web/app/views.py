a12b42f6ad19e782d6ede2efa7f528d96f9b175b
Victoria <victoriastaada@gmail.com>
2021-04-13 20:40:42 +0000

    swapped received_messages with watts and temperature data

--- web/app/views.py
+++ web/app/views.py
@@ -64,11 +64,14 @@ def explorer(request):
     context = {}
     context['segment'] = 'explorer'
     
-    msg_json = requests.get('http://192.168.1.123:9090/api/v1/query_range?query=received_messages&start=1618172204&end=1618191179&step=20s').json()
+    # msg_json = requests.get('http://192.168.1.123:9090/api/v1/query_range?query=received_messages&start=1618172204&end=1618191179&step=20s').json()
+    watt = requests.get('http://192.168.1.123:9090/api/v1/query_range?query=watts&start=1618172204&end=1618191179&step=20s').json()
+    temp = requests.get('http://192.168.1.123:9090/api/v1/query_range?query=temperature&start=1618172204&end=1618191179&step=20s').json()
+    wattTemp = watt['data']['result'] + temp['data']['result']
     mqtt_list = []
-    for i in range(len(msg_json['data']['result'])):
-        msg_values = dict(msg_json['data']['result'][i]['values'])
-        topic = msg_json['data']['result'][i]['metric']['topic']
+    for i in range(len(wattTemp)):
+        msg_values = dict(wattTemp[i]['values'])
+        topic = wattTemp[i]['metric']['topic']
         msg_values = [(datetime.utcfromtimestamp(key).strftime('%Y-%m-%d %H:%M:%S'), topic, topic.rsplit("/", 1)[1], int(value)) for key, value in msg_values.items()]
         mqtt_list += msg_values
     
